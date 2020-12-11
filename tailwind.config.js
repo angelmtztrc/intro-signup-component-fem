a4023a8139a351420a1139e46b8bed551e27bc2d
Angel Martinez <angel.martineztrc@gmail.com>
2020-12-11 00:35:36 -0600

    custom colors and fonts added to tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,21 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-red': 'hsl(0, 100%, 74%)',
+        'primary-green': 'hsl(154, 59%, 51%)',
+        'accent-blue': 'hsl(248, 32%, 49%)',
+        'neutral-darkblue': 'hsl(249, 10%, 26%)',
+        'neutral-grayishblue': 'hsl(246, 25%, 77%)'
+      },
+      fontFamily: {
+        body: ['Poppins', 'sans-serif']
+      }
+    }
   },
   variants: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
-}
+  plugins: []
+};
